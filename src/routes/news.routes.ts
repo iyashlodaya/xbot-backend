32b87dea59b5d3eeaa88d6c2154a18b92d201bfc
Yash Lodaya <iyashlodaya@gmail.com>
2025-02-16 23:51:10 +0530

    fix: added verifyToken middleware for news routes.

--- src/routes/news.routes.ts
+++ src/routes/news.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { getTrendingTopics } from "../controllers/news/news.controller";
+import verifyToken from "../middleware/verify.token";
 
 const router = Router();
 
-router.get("/trending", getTrendingTopics);
+router.get("/trending", verifyToken, getTrendingTopics);
 
 export default router;
